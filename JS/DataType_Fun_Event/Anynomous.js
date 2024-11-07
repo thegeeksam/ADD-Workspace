8c0baab3e2135c77faf367777125658a12cc106c
Sampath Kumar S <29780018+thegeeksam@users.noreply.github.com>
2024-11-07 23:04:45 +0530

    Delete JS directory

--- JS/DataType_Fun_Event/Anynomous.js
+++
@@ -1,9 +0,0 @@
-//Anonymous functions
-let x = function (x, y){   
-        console.log(x + y);
-        console.log("I'm anonymous function!");
-    };
-
-console.log(typeof(x));
-//call the anonymous function
-x(5,10);    
